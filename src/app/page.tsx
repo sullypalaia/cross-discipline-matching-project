03c0946b1bcdab84c478f3767fe1f5e6c51aa0d2
Sully Palaia <sullypalaia@gmail.com>
2026-09-05 15:51:51 -0700

    added supabase table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 import App from "../App";
+import { cookies } from "next/headers";
+import { createClient } from "@/utils/supabase/server";
+import type { Project } from "../components/ProjectCard";
 
-export default function Home() {
-  return <App />;
+export default async function Home() {
+  const cookieStore = await cookies();
+  const supabase = createClient(cookieStore);
+  const { data: projects, error } = await supabase
+    .from("user_projects")
+    .select();
+
+  if (error) {
+    console.log(error);
+    throw error;
+  }
+
+  return <App projects={(projects ?? []) as Project[]} />;
 }
