463ac4da44baa62d98d9aad4045cc6fb21802617
jakeorch <jakeaorchanian@gmail.com>
2026-09-06 10:44:35 -0700

    remove placeholder files

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
 import { createClient } from "@/utils/supabase/clients";
